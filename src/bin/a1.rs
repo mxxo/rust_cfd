5b8a7a6da2bdf42db3c9b2b77f15b1fc1c9ae378
Max Orok <morok@mevex.com>
2019-09-24 16:11:32 -0400

    Set ICs in solution

--- src/bin/a1.rs
+++ src/bin/a1.rs
@@ -9,7 +9,7 @@
 // advance each cell average
 
 // general solution code
-use rust_cfd::{Solution1d};
+use rust_cfd::Solution1d;
 
 // euler's number e
 use std::f64;
@@ -19,11 +19,10 @@ use gauss_quad::Midpoint;
 
 // specific solution
 fn main() {
-
     // discretize domain
     let left = -5.0;
     let right = 5.0;
-    let num_cells = 3;
+    let num_cells = 13;
 
     // initialize the different flux functions
     //let left_cell = |left, right, flux_fn: Fn(f64) -> f64| flux_fn(left);
@@ -33,25 +32,24 @@ fn main() {
     dbg!(10 % 10);
 
     // make a new, empty solution
-    let soln = Solution1d::new(left, right, num_cells);
-    //dbg!(soln);
+    let mut soln = Solution1d::new(left, right, num_cells);
+    dbg!(&soln);
 
     // PDE number 1
     // update the solution with initial conditions
-    let ic_1 = |x: f64| if x.abs() < 2.0 {
-                            (-x * x).exp()
-                        } else {
-                            0.
-                        };
+    let ic_1 = |x: f64| if x.abs() < 2.0 { (-x * x).exp() } else { 0. };
 
     dbg!(ic_1(2.));
     dbg!(ic_1(0.2));
     dbg!(ic_1(0.));
 
     let quad = Midpoint::init(10);
-    dbg!(quad.integrate(-1.0, 1.0, |x| x*x));
+    dbg!(quad.integrate(-1.0, 1.0, |x| x * x));
 
-    //soln.update();
+    soln.init(ic_1);
+    dbg!(&soln);
+
+    soln.print();
 
     let t_end = 10.;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@
 //-----------------------------------------------------------------------------
 // linear algebra
 extern crate nalgebra as na;
+
 // floating-point equality
 extern crate approx;
 
@@ -21,14 +22,9 @@ extern crate approx;
 extern crate itertools_num;
 use itertools_num::linspace;
 
-// index operator overloading
-extern crate derive_more;
-use derive_more::Index;
-
 // numerical integration
 extern crate gauss_quad;
 use gauss_quad::Midpoint;
-
 
 // pub fn make_flux_fn<F> (left: f64, right: f64, flux_fn: Fn(f64, f64) -> f64) {
 //     match flux_type {
@@ -40,15 +36,50 @@ use gauss_quad::Midpoint;
 // }
 
 fn riemann_soln(left: f64, right: f64) -> f64 {
-
     unimplemented!();
+}
+
+// a discrete solution cell
+#[derive(Debug)]
+struct Cell1d {
+    pub value: f64,
+    pub left: f64,
+    pub right: f64,
+}
+
+// a boundary | cell interface
+#[derive(Debug)]
+struct Boundary1d {
+    pub coord: f64,
+    pub left_cell: usize,
+    pub right_cell: usize,
+}
+
+// create `num_cell` equally sized pieces between left and right
+fn make_domain(left: f64, right: f64, num_cells: usize) -> Vec<Boundary1d> {
+    // one more boundary than number of cells
+    let boundary_coords = linspace::<f64>(left, right, num_cells + 1);
+    let mut boundaries = Vec::new();
+
+    for (idx, coord) in boundary_coords.enumerate() {
+        // num_cells is for wraparound indexing of usize values
+        let prev_idx = if idx == 0 {num_cells - 1} else { idx - 1 };
+        let next_idx = idx % num_cells;
+        boundaries.push(Boundary1d {
+            coord,
+            left_cell: prev_idx,
+            right_cell: next_idx,
+        });
+    }
+
+    boundaries
 }
 
 // the solution vector
 #[derive(Debug)]
 pub struct Solution1d {
     // cell values
-    cells: Vec<f64>,
+    cells: Vec<Cell1d>,
     // the discretized domain
     domain: Vec<Boundary1d>,
 }
@@ -56,84 +87,75 @@ pub struct Solution1d {
 impl Solution1d {
     // constructor
     pub fn new(left: f64, right: f64, num_cells: usize) -> Self {
-
         // initialize the domain
-        let mut domain = Vec::new();
+        let domain = make_domain(left, right, num_cells);
+        // reserve space for the cell vector
+        let mut cells = Vec::with_capacity(num_cells);
+
+        for cell_idx in 0..num_cells {
+            cells.push(Cell1d {
+                value: 0.0,
+                left: domain[cell_idx].coord,
+                right: domain[cell_idx + 1].coord,
+            });
+        }
 
         Solution1d {
-            cells: vec![0.0; num_cells], // 0 out the solution vector
+            cells,
             domain,
         }
     }
 
-    // initialize the solution vector with some initial condition expression
-    pub fn init<F> (&mut self, ic: F)
-        where F: Fn(f64) -> f64 {
-            // hardcode averaging scheme for simplicity
-            let quad = Midpoint::init(self.cells.len());
-
+    // initialize the solution vector using an initial condition expression
+    pub fn init<F>(&mut self, ic_fn: F)
+    where
+        F: Fn(f64) -> f64,
+    {
+        // hardcode averaging scheme for simplicity
+        let quad = Midpoint::init(1000);
+        // average initial condition in each cell
+        for mut cell in self.cells.iter_mut() {
+            cell.value = quad.integrate(cell.left, cell.right, &ic_fn);
+        }
     }
 
     // update the solution using some flux function
-    pub fn update<F> (&mut self, flux_fn: F)
-        where F: Fn(f64) -> f64 {
+    pub fn update<F>(&mut self, flux_fn: F)
+    where
+        F: Fn(f64, f64) -> f64,
+    {
 
     }
 
+    // print out the solution to csv
+    pub fn print(&self) {
+        println!("position, value");
+        for cell in &self.cells {
+            println!("{}, {}", 0.5 * (cell.left + cell.right), cell.value);
+        }
+    }
 }
-
-// a boundary -- a cell interface
-#[derive(Debug)]
-struct Boundary1d {
-    coord: f64,
-    left_cell: usize,
-    right_cell: usize,
-}
-
-
-//    // create `num_cell` equally sized pieces between left and right
-//    pub fn create(left: f64, right: f64, num_cells: usize) -> Self {
-//        // one more boundary than number of cells
-//        let boundary_coords = linspace::<f64>(left, right, num_cells + 1);
-//        let mut boundaries = Vec::new();
-//
-//        for (idx, coord) in boundary_coords.enumerate() {
-//            // (-1 + num_cells is for wraparound indexing)
-//            let prev_idx = if idx == 0 {num_cells - 1} else {idx - 1};
-//            let next_idx = (idx + 1) % (num_cells + 1);
-//            boundaries.push(Boundary1D {
-//                coord,
-//                left_cell: prev_idx,
-//                right_cell: next_idx,
-//            });
-//        }
-//
-//        PeriodicDomain1d {
-//            boundaries,
-//        }
-//    }
-//}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    pub fn periodic() {
-        let l = 10;
-        let domain = PeriodicDomain1d::even(-10., 10., l);
-        let left = 0;
-        let right = domain.len() - 1;
-        // left periodic
-        assert!(domain.prev_idx(left) == right);
-        // right periodic
-        assert!(domain.next_idx(right) == left);
-    }
+    //#[test]
+    //pub fn periodic() {
+    //    let l = 10;
+    //    let domain = PeriodicDomain1d::even(-10., 10., l);
+    //    let left = 0;
+    //    let right = domain.len() - 1;
+    //    // left periodic
+    //    assert!(domain.prev_idx(left) == right);
+    //    // right periodic
+    //    assert!(domain.next_idx(right) == left);
+    //}
 
     #[test]
     pub fn domain_size() {
         let l = 100;
-        let domain = PeriodicDomain1d::even(-10., 10., l);
+        let domain = make_domain(-10., 10., l);
         assert!(domain.len() == l + 1);
     }
 }
