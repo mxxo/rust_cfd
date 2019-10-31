312503b4d92071ea5089e4b5d505ec723be1112b
Max Orok <morok028@uottawa.ca>
2019-10-31 18:24:34 -0400

    Flesh out Euler state functions

--- src/bin/a2.rs
+++ src/bin/a2.rs
@@ -19,43 +19,42 @@
 use rust_cfd::riemann::{solve_euler, EulerState};
 
 fn main() {
-    
-    const GAMMA: f64 = 1.4; 
+    const GAMMA: f64 = 1.4;
 
     // case 1
     let left_state = EulerState {
         density: 2.281,
         velocity: 164.83,
         pressure: 201.17e3,
-        gamma: GAMMA
+        gamma: GAMMA,
     };
 
     let right_state = EulerState {
         density: 1.408,
         velocity: 0.0,
         pressure: 101.1e3,
-        gamma: GAMMA
+        gamma: GAMMA,
     };
-    
-    let t_final = 12e-3; // s 
+
+    let t_final = 12e-3; // s
 
     dbg!(&left_state);
     dbg!(&right_state);
-    
-    solve_euler(left_state, right_state, t_final); 
+
+    solve_euler(left_state, right_state, t_final);
 
     // case 2
     let left_state = EulerState {
         density: 1.045,
         velocity: 200.0,
         pressure: 300e3,
-        gamma: GAMMA, 
+        gamma: GAMMA,
     };
     let right_state = EulerState {
         density: 3.483,
         velocity: 200.0,
         pressure: 300e3,
-        gamma: GAMMA, 
+        gamma: GAMMA,
     };
 
     let t_final = 25e-3; // s
@@ -63,20 +62,20 @@ fn main() {
     dbg!(&left_state);
     dbg!(&right_state);
 
-    solve_euler(left_state, right_state, t_final); 
-    
+    solve_euler(left_state, right_state, t_final);
+
     // case 3
     let left_state = EulerState {
         density: 1.598,
         velocity: -383.64,
         pressure: 91.88e3,
-        gamma: GAMMA, 
+        gamma: GAMMA,
     };
     let right_state = EulerState {
         density: 2.787,
         velocity: -216.97,
         pressure: 200e3,
-        gamma: GAMMA, 
+        gamma: GAMMA,
     };
 
     let t_final = 35e-3; // s
@@ -84,20 +83,20 @@ fn main() {
     dbg!(&left_state);
     dbg!(&right_state);
 
-    solve_euler(left_state, right_state, t_final); 
+    solve_euler(left_state, right_state, t_final);
 
     // case 4
     let left_state = EulerState {
         density: 4.696,
         velocity: 0.0,
         pressure: 404.4e3,
-        gamma: GAMMA, 
+        gamma: GAMMA,
     };
     let right_state = EulerState {
         density: 1.408,
         velocity: 0.0,
         pressure: 101.1e3,
-        gamma: GAMMA, 
+        gamma: GAMMA,
     };
 
     let t_final = 7e-3; // s
@@ -105,5 +104,5 @@ fn main() {
     dbg!(&left_state);
     dbg!(&right_state);
 
-    solve_euler(left_state, right_state, t_final); 
+    solve_euler(left_state, right_state, t_final);
 }
